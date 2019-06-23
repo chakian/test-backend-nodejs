14afaf293c4771e808505e5ec632e5078d3b6add
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-06-23 18:34:51 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,23 +6,11 @@ var config = {
             db: 'test-case-study',
             user: '',
             password: ''
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '3000'
         }
     },
     production: {
         database: {
-            host: 'localhost',
-            port: '27017',
-            db: 'test-case-study',
-            user: '',
-            password: ''
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '1234'
+            use_env_variable: 'MONGODB_URI'
         }
     }
 };
