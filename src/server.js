b90c432463b64f6fd6b3c21b64347a34c082d5be
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-06-23 11:00:41 +0300

    basic routing is handled. there's no error handling yet.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('This server only accepts POST requests!');
 });
-var server = app.listen(3000, function () { });
+app.delete('/', function (req, res) {
+    res.send('This server only accepts POST requests!');
+});
+app.put('/', function (req, res) {
+    res.send('This server only accepts POST requests!');
+});
+
+app.post('/fetch-todo', function(req, res) {
+    const fetchTodos = require('../src/fetchTodos');
+
+    const requestParams = {
+        startDate: "2019-01-01"
+    };
+
+    var result = fetchTodos(requestParams);
+
+    res.send(result);
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Server listening at http://%s:%s", host, port);
+});
